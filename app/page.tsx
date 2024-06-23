0e080f70ff4fcb1131b339d5e7ad753355a7a1c2
PierreDAVOINE <davoine.pierre@hotmail.fr>
2024-06-23 17:05:54 +0200

    Changement du favico, ajout du lien depuis la page d'acceuil

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { FaArrowCircleRight } from 'react-icons/fa';
+
 export default function Home() {
   return (
-    <main className="h-screen flex justify-center items-center font-sans">
-      <h1 className="text-2xl ">Welcome</h1>
+    <main className="h-screen flex flex-col gap-6 justify-center items-center font-sans">
+      <h1 className="text-2xl ">Welcome to the best todo list app !</h1>
+      <Button className="text-3xl py-9 px-9" asChild>
+        <Link href="/todolist">
+          Go to my todo-list
+          <FaArrowCircleRight className="ml-6" />
+        </Link>
+      </Button>
     </main>
   );
 }
