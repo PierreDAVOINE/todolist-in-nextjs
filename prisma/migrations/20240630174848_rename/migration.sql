dca35e9116389483284fdd04ef90cf2874750171
PierreDAVOINE <davoine.pierre@hotmail.fr>
2024-06-30 20:17:17 +0200

    Ajout de prisma + initialisation

---
+++ prisma/migrations/20240630174848_rename/migration.sql
@@ -0,0 +1,18 @@
+/*
+  Warnings:
+
+  - You are about to drop the `tasks` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "tasks";
+PRAGMA foreign_keys=on;
+
+-- CreateTable
+CREATE TABLE "Tasks" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "done" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
